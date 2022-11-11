997014d214f1f4c44d3a1253acf13140f3220bfa
Jigar Patel <jpxtel98@outlook.com>
2022-11-10 20:23:40 -0500

    Add code to generate AWS upload url

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ dotenv.config()
 import cors from 'cors';
 import express from 'express';
 const app = express();
+import { generateUploadURL } from './s3.js'
 
 const PORT = process.env.PORT || 5050
 
 app.use(cors());
 app.use(express.static('public'))
+
+app.get('/get-s3-url', async (req, res) => {
+    const url = await generateUploadURL()
+    res.send({ url })
+})
 
 app.listen(PORT, () =>
     console.log(`Server listening on port: ${PORT} 🚀🚀`)
---
+++ s3.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv'
+import aws from 'aws-sdk'
+import { v4 as uuidv4 } from 'uuid'
+dotenv.config()
+
+const region = process.env.AWS_BUCKET_REGION
+const bucketName = process.env.AWS_BUCKET_NAME
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+
+const s3 = new aws.S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: 'v4'
+})
+
+export async function generateUploadURL() {
+
+  const imageName = uuidv4()
+  const params = ({
+    Bucket: bucketName,
+    Key: imageName,
+    Expires: 60
+  })
+
+  const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+  return uploadURL
+}
